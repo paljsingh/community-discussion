edb9de3028f9e7a35e98c02dbdcde93b510df7d7
jitender.singh <jitender.singh@salesforce.com>
2021-07-16 20:50:45 +0530

    moved config to common location

--- backend/users/config.py
+++ backend/users/config.py
@@ -5,5 +5,5 @@ class Config:
 
     @classmethod
     def load(cls):
-        with open('config.yaml', 'r') as file:
+        with open('../config.yaml', 'r') as file:
             return yaml.load(file, Loader=yaml.FullLoader) or {}
