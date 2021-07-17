b7b40ba7efacf607574b9f2c4994a80d5e125955
jitender.singh <jitender.singh@salesforce.com>
2021-07-18 00:23:13 +0530

    tail the logs

--- run-backend.sh
+++ run-backend.sh
@@ -3,10 +3,15 @@ declare -a components=("users")
 
 flask_run_port=${FLASK_RUN_PORT:-5000}
 flask_env=${FLASK_ENV:-development}
+
+log_files=""
 for i in ${components[@]}; do
   cd "backend/$i" 1>/dev/null
   echo "running backend component '$i' on port $flask_run_port ..."
   FLASK_ENV=$flask_env FLASK_RUN_PORT=$flask_run_port FLASK_APP=app.py flask run &
   flask_run_port=$(expr $flask_run_port + 1)
+  log_files="$log_files backend/$i/logs/c18n.log"
   cd - 1>/dev/null
 done
+
+tail -F $(echo $log_files)
