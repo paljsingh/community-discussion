0463ab88e969cbfaba35617b098647f83a6004f9
jitender.singh <jitender.singh@salesforce.com>
2021-07-31 00:17:22 +0530

    chat app with websockets

---
+++ backend/chat/app.py
@@ -0,0 +1,53 @@
+import os
+
+from flask import Flask, render_template, session, copy_current_request_context
+from flask_socketio import SocketIO, emit, disconnect
+from threading import Lock
+import logging
+logging.basicConfig(level=logging.DEBUG)
+
+
+async_mode = 'gevent'
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'secret!'
+socket_ = SocketIO(app, async_mode=async_mode, cors_allowed_origins=["http://localhost:8080", "http://localhost:8081",
+                                                                     'http://localhost:5010'])
+thread = None
+thread_lock = Lock()
+
+
+@app.route('/')
+def index():
+    return render_template('index.html', async_mode=socket_.async_mode)
+
+
+@socket_.on('my_event', namespace='/test')
+def test_message(message):
+    session['receive_count'] = session.get('receive_count', 0) + 1
+    emit('my_response',
+         {'data': message['data'], 'count': session['receive_count']})
+
+
+@socket_.on('my_broadcast_event', namespace='/test')
+def test_broadcast_message(message):
+    session['receive_count'] = session.get('receive_count', 0) + 1
+    emit('my_response',
+         {'data': message['data'], 'count': session['receive_count']},
+         broadcast=True)
+
+
+@socket_.on('disconnect_request', namespace='/test')
+def disconnect_request():
+    @copy_current_request_context
+    def can_disconnect():
+        disconnect()
+
+    session['receive_count'] = session.get('receive_count', 0) + 1
+    emit('my_response',
+         {'data': 'Disconnected!', 'count': session['receive_count']},
+         callback=can_disconnect)
+
+
+if __name__ == '__main__':
+    port = int(os.environ.get("WEBSOCKET_PORT", 6000))
+    socket_.run(app, debug=True, port=port)
