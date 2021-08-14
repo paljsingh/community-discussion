437cb63caa9ba8f41af10209ddef82eb4b25c846
jitender.singh <jitender.singh@salesforce.com>
2021-08-14 20:49:38 +0530

    usergroup apis

--- backend/usergroups/app.py
+++ backend/usergroups/app.py
@@ -1,7 +1,10 @@
+from pymodm import MongoModel
+from pymodm.fields import CharField, DateTimeField, ListField
+from pymongo.write_concern import WriteConcern
+
 import logging.config
 from datetime import datetime
 from random import Random
-from typing import List
 
 from werkzeug.exceptions import HTTPException
 from faker import Faker
@@ -10,15 +13,18 @@ import uuid
 
 from common.customflask import CustomFlask
 from common.customverifier import CustomJWTVerifier
+from common.db import Db
 from common.utils import FlaskUtils
+from communities.app import Community
 
 logging.config.fileConfig('../logging.conf')
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
 app = CustomFlask(__name__)
-conf = app.conf
-db = app.db
+app.config['MONGODB_SETTINGS'] = {'host': app.conf.get('db_uri')}
+db = Db()
+
 
 for cls in HTTPException.__subclasses__():
     app.register_error_handler(cls, CustomJWTVerifier.handle_error)
@@ -27,17 +33,20 @@ for cls in HTTPException.__subclasses__():
 @app.route("/api/v1/communities/<community_id>/usergroups/new", methods=['POST'])
 @CustomJWTVerifier.verify_jwt_token
 def create_usergroup(community_id, my_id, is_admin=False):
-
-    new_usergroup = Usergroup(my_id).fake_info()
-
     data = request.get_json()
+    new_usergroup = Usergroup(created_by=my_id)
+    new_usergroup.fake_info()
+
     # override with any data available in the post body.
     if data and data.get('name'):
-        new_usergroup.with_name(data.get('name'))
+        new_usergroup.name = data['name']
+    if data and data.get('tags'):
+        new_usergroup.tags = data['tags']
     if data and data.get('users'):
-        new_usergroup.with_users(data.get('users'))
+        new_usergroup.users = data['users']
+
     new_usergroup.save()
-    return app.make_response(new_usergroup.__dict__())
+    return app.make_response(new_usergroup.to_son())
 
 
 @app.route("/api/v1/communities/<community_id>/usergroups", methods=['GET'])
@@ -50,60 +59,55 @@ def get_usergroups(community_id, my_id, is_admin=False):
     for non-admins, return only the public data.
     :return:
     """
-    community = db.retrieve('communities', {'_id': community_id}, limit=1)
-    usergroup_ids = community['usergroups']
 
-    skip, limit = FlaskUtils.get_skip_limit()
+    # find all usergroup ids in the community
+    community = db.get(Community, community_id, to_son=False)
+    usergroup_ids = community.usergroups
+
+    select_columns = ["name", "users"]
     if is_admin:
-        usergroups = db.retrieve("usergroups", filters={'_id': {'$in': usergroup_ids}}, skip=skip, limit=limit)
-    else:
-        usergroups = db.retrieve("usergroups", filters={'_id': {'$in': usergroup_ids}},
-                                 select_columns=["_id", "name", "users"], skip=skip, limit=limit)
+        select_columns = None
 
-    for ug in usergroups:
-        for user in ug.get('users'):
-            filtered_user = {"_id": user.get("_id"), "name": user.get("name")}
-            ug['users'] = filtered_user
-    # send response
+    # not using auto_dereferencing, instead create a bulk query to fetch all usergroups together.
+
+    # collect usergroups for the above usergroup ids.
+    usergroups = db.retrieve(
+        Usergroup, filters={'id': {'$in': usergroup_ids}},
+        select_columns=select_columns)
+
     return app.make_response(usergroups)
 
 
 @app.route('/api/v1/usergroups/search', methods=['GET'])
 @CustomJWTVerifier.verify_jwt_token
-def search_users(my_id, is_admin=False):
-    select_columns = []
-    if not is_admin:
-        select_columns = ['_id', 'name']
-
-    # The comma is not a typo.
-    # get_url_args returns a tuple, the syntax is needed to expand it inline when receiving a single argument.
+def search_usergroups(my_id, is_admin=False):
     name, = FlaskUtils.get_url_args('name')
 
-    skip, limit = FlaskUtils.get_skip_limit()
-
     # search for given name in indexed text-fields
-    users = db.retrieve('users', {
+    usergroups = db.retrieve(Usergroup, {
         '$text': {
             '$search': name,
             '$caseSensitive': False,
             '$diacriticSensitive': False,   # treat é, ê the same as e
         }
-    }, select_columns=select_columns, limit=limit, skip=skip)
+    })
 
     # TODO: any history updates / events here.
 
-    return app.make_response(users)
+    return app.make_response(usergroups)
+
+# TODO
+# api to fetch my user groups
+# api to update user group info
 
 
-class Usergroup:
-
-    def __init__(self, created_by):
-        self._id = '{}'.format(uuid.uuid4())
-        self.name = None
-        self.users = set()
-        self.creation_date = datetime.utcnow().timestamp()
-        self.created_by = created_by
-        pass
+class Usergroup(MongoModel):
+    id = CharField(required=True, primary_key=True, default=uuid.uuid4)
+    name = CharField(required=True)
+    users = ListField(required=False, blank=True)
+    creation_date = DateTimeField(required=True, default=datetime.utcnow)
+    created_by = CharField(required=True)
+    tags = ListField(required=False, blank=True)
 
     def fake_info(self):
         f = Faker()
@@ -111,25 +115,6 @@ class Usergroup:
         self.name = ' '.join([str.title(f.words()) for i in range(Random().randint(1, 3))])
         return self
 
-    def with_name(self, name: str):
-        self.name = name
-        return self
-
-    def with_users(self, users: List):
-        self.users.update(users)
-        return self
-
-    def __dict__(self):
-        return {
-            '_id': self._id,
-            'name': self.name,
-            'creation_date': self.creation_date,
-            'created_by': self.created_by
-        }
-
-    def save(self):
-        db.save(self.__dict__(), 'usergroups')
-
-    @staticmethod
-    def get(usergroup_id: str, select_columns: List = None):
-        return db.retrieve('usergroups', filters={'_id': usergroup_id}, select_columns=select_columns, limit=1)
+    class Meta:
+        write_concern = WriteConcern(j=True)
+        connection_alias = 'my-app'
